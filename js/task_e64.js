1c10c1a09226ae3730b777ddb5ed2b6a6235a73b
Rman vsc <rom38@users.noreply.github.com>
2023-02-19 13:25:32 +0000

    start proj

---
+++ js/task_e64.js
@@ -0,0 +1,42 @@
+const XMLText =`<list>
+<student>
+  <name lang="en">
+    <first>Ivan</first>
+    <second>Ivanov</second>
+  </name>
+  <age>35</age>
+  <prof>teacher</prof>
+</student>
+<student>
+  <name lang="ru">
+    <first>Петр</first>
+    <second>Петров</second>
+  </name>
+  <age>58</age>
+  <prof>driver</prof>
+</student>
+</list>`
+
+// let parser = new DOMParser()
+// let XMLTextParse = parser.parseFromString(XMLText)
+// console.log(XMLTextParse)
+
+const jsonString = `{
+    "list": [
+     {
+      "name": "Petr",
+      "age": "20",
+      "prof": "mechanic"
+     },
+     {
+      "name": "Vova",
+      "age": "60",
+      "prof": "pilot"
+     }
+    ]
+   }`
+
+let data = JSON.parse(jsonString)
+console.log(data.list)
+console.log(data.list[1].name)
+debugger
